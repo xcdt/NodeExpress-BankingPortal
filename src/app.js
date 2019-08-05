ac8f653407106e851c88976f018c944d0e28b6c1
Francisco Luis Fernandez-Trujillo <Francisco.fernandez@autovistagroup.com>
2019-08-05 08:39:34 +0200

    module completed

--- src/app.js
+++ src/app.js
@@ -12,6 +12,8 @@ app.set('view engine', 'ejs');
 // configuring the statis directory (css, js, etc)
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+
 const accountData = fs.readFileSync(
   path.join(__dirname, 'json', 'accounts.json'), 'utf8'
 );
@@ -22,7 +24,7 @@ const userData = fs.readFileSync(
   path.join(__dirname, 'json', 'users.json'), 'utf8'
 );
 
-const users =  JSON.parse(userData);
+const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts });
@@ -40,8 +42,33 @@ app.get('/credit', (req, res) => {
   res.render('account', { account: accounts.credit });
 });
 
-app.get('/profile', (req, res)  => {
+app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
+});
+
+app.get('/transfer', (req, res) =>{
+  res.render('transfer');
+});
+
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance -= req.body.amount;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance, 10)
+  + parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json', 'acccounts.json'), accountsJSON, 'utf8');
+  res.render('payment', { message: ' Payment Successful', account: accounts.credit });
 });
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
