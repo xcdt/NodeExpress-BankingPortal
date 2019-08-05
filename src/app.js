12d103c9b334c213d0573799c967b8d21bf3cf98
Francisco Luis Fernandez-Trujillo <Francisco.fernandez@autovistagroup.com>
2019-08-05 08:42:34 +0200

    modifying path for writeFileSync

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.post('/transfer', (req, res) => {
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance, 10)
   + parseInt(req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
   res.render('transfer', { message: 'Transfer Completed' });
 });
 
