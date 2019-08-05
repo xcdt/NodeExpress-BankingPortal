134a13da4c565f08e94bd32c7cfcfae5aec02dcd
Francisco Luis Fernandez-Trujillo <Francisco.fernandez@autovistagroup.com>
2019-08-05 08:50:54 +0200

    correcting error in writeFileSync

--- src/app.js
+++ src/app.js
@@ -55,6 +55,8 @@ app.post('/transfer', (req, res) => {
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance, 10)
   + parseInt(req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts, null, 4);
+  console.log('HOolaa');
+  console.log(__dirname);
   fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
   res.render('transfer', { message: 'Transfer Completed' });
 });
@@ -67,7 +69,7 @@ app.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json', 'acccounts.json'), accountsJSON, 'utf8');
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
   res.render('payment', { message: ' Payment Successful', account: accounts.credit });
 });
 
