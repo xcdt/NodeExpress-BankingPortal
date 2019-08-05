63ff27c8b0bc3c11a84dbe3e4b712febfb762b00
Francisco Luis Fernandez-Trujillo <Francisco.fernandez@autovistagroup.com>
2019-08-05 09:29:21 +0200

    creating a data access library

--- src/app.js
+++ src/app.js
@@ -5,26 +5,16 @@ const express = require('express');
 
 const app = express();
 
+const { accounts, users, writeJSON } = require('./data');
+
 // setting views directory
 app.set('views', path.join(__dirname, 'views'));
 // setting the views engine
 app.set('view engine', 'ejs');
-// configuring the statis directory (css, js, etc)
+// configuring the static directory (css, js, etc)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
-
-const accountData = fs.readFileSync(
-  path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-);
-
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(
-  path.join(__dirname, 'json', 'users.json'), 'utf8'
-);
-
-const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts });
@@ -54,8 +44,7 @@ app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance -= req.body.amount;
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance, 10)
   + parseInt(req.body.amount, 10);
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('transfer', { message: 'Transfer Completed' });
 });
 
@@ -66,8 +55,7 @@ app.get('/payment', (req, res) => {
 app.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount, 10);
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('payment', { message: ' Payment Successful', account: accounts.credit });
 });
 
